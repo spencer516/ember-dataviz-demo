109cbd1a42279003672c7d67917058b3cea3a11b
Spencer Price <spencer516@gmail.com>
2018-10-02 07:42:09 -0400

    add slides

--- app/controllers/demo-2.js
+++
@@ -1,6 +0,0 @@
-import Controller from '@ember/controller';
-
-export default class Demo2 extends Controller {
-  color = 'blue';
-  angle = 0;
-}
--- app/utils/transparentize.js
+++
@@ -1,7 +0,0 @@
-import { color } from 'd3-color';
-
-export default function transparentize(colorStr, opacity) {
-  const colorObj = color(colorStr);
-  colorObj.opacity = opacity;
-  return colorObj.toString();
-}
